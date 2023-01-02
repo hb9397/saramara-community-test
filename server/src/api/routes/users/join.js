3f2c36099b01c40f14e0836657323aa5dad54381
hb9397 <hb9397@naver.com>
2023-01-02 18:09:33 +0900

    회원가입 라우터 api 수정

--- server/src/api/routes/users/join.js
+++ server/src/api/routes/users/join.js
@@ -1,5 +1,5 @@
 /* 
- * /users/ 와 관련된 routes 
+ * api/join router
  */
 import { Router } from "express";
 import Users from "../../../models/users.js";
@@ -11,8 +11,8 @@ const router = Router();
 const join = (app) => {
     // 매개변수로 받은 최상위 routes에 index.js를 라우터로 사용하기 위한 Router() 함수를 app으로 받아 
     // 현재 파일을 Router()를 이용해서 라우터로 만들고 최상위 index로 부터 받은 Router() = app 함수를 사용해 현재 파일을 라우터로 등록
-    app.use(router);
-    
+    app.use('/register',router);
+
     // 회원가입 처리 api - middleware 디렉토리 이동 의심 발생
     // TODO : 가입된 회원인지 검증하는 로직 미처리
     router.post("/join", async(req, res, next) => {
